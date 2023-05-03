4d2c80049b3628d4b1601c9a6b4127a87e8a9d50
Tory <88531407+1two13@users.noreply.github.com>
2023-05-04 01:20:07 +0900

    feat: 상수 추가

--- src/static/constants.ts
+++ src/static/constants.ts
@@ -1,5 +1,6 @@
 const BASE_URL = '/api/v1/search-conditions';
 const FIRST_TITLE = '국내 모든 임상시험 검색하고';
 const SECOND_TITLE = '온라인으로 참여하기';
+const EXPIRATION_TIME_IN_MS = 43200000; // 12시간
 
-export { BASE_URL, FIRST_TITLE, SECOND_TITLE };
+export { BASE_URL, FIRST_TITLE, SECOND_TITLE, EXPIRATION_TIME_IN_MS };
