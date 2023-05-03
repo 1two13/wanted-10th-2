612d63da1123614307a50cb757df1abfd1a05e3e
Tory <88531407+1two13@users.noreply.github.com>
2023-05-04 01:19:25 +0900

    feat: 추천 검색어 있을 때랑 없을 때 UI 분리

--- src/components/SuggestedList.tsx
+++ src/components/SuggestedList.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import SuggestedItem from './SuggestedItem';
+import SearchSvg from './SearchSvg';
 
-function SuggestedList() {
 function SuggestedList({ suggestedList, selectedIndex }: any) {
   return (
-    <Div>
-      <div>
-        <SuggestedItem />
-        <SuggestedItem />
-      </div>
-    </Div>
+    <div>
+      {suggestedList.length > 0 ? (
+        <Div>
+          <div>
+            {suggestedList.map((el: any, index: number) => (
+              <SuggestedItem key={el.id} isFocus={index === selectedIndex}>
+                <div>
+                  <SearchSvg />
+                </div>
+                {el.name}
+              </SuggestedItem>
+            ))}
+          </div>
+        </Div>
+      ) : (
+        <Div>
+          <div>
+            <SuggestedItem>
+              <div>검색어 없음</div>
+            </SuggestedItem>
+          </div>
+        </Div>
+      )}
+    </div>
   );
 }
 
@@ -27,4 +44,15 @@ const Div = styled.div`
   background-color: #ffffff;
   font-size: 16px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
+
+const SuggestedItem = styled.div<{ isFocus?: boolean }>`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15%;
+  border-bottom: ${props => (props.isFocus ? '2px solid #007be9' : '#fff')};
+  & > div {
+    margin-right: 13px;
+    color: #a7afb7;
+  }
 `;
