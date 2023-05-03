6a6e0ff40f240fed68ddb06320afba9160305a40
Tory <88531407+1two13@users.noreply.github.com>
2023-05-03 13:31:46 +0900

    feat: input 창 클릭했을 때 부모의 border 색상 변경

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
+
 import SearchSvg from './SearchSvg';
 
 function SearchBar() {
+  const [isClicked, setIsClicked] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (inputRef.current && !(inputRef.current as any).contains(e.target)) {
+        setIsClicked(false);
+      }
+    };
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, []);
+
+  const onClickInput = () => setIsClicked(true);
+
   return (
-    <Div>
+    <Div isClicked={isClicked}>
       <Form>
         <div>
           <SearchSvg />
         </div>
-        <Input placeholder="질환명을 입력해 주세요." />
+        <Input placeholder="질환명을 입력해 주세요." ref={inputRef} onClick={onClickInput} />
       </Form>
       <Svg>
         <SearchSvg />
@@ -20,7 +36,11 @@ function SearchBar() {
 
 export default SearchBar;
 
-const Div = styled.div`
+interface DivProps {
+  isClicked: boolean;
+}
+
+const Div = styled.div<DivProps>`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -28,8 +48,7 @@ const Div = styled.div`
   height: 70px;
   margin: 0 auto;
   border-radius: 42px;
-  border: 2px solid;
-  border-color: #ffffff;
+  border: 2px solid ${props => (props.isClicked ? '#007be9' : '#ffffff')};
   background-color: #ffffff;
 `;
 
@@ -38,17 +57,15 @@ const Form = styled.form`
   height: 100%;
   color: #a7afb7;
   display: flex;
+  align-items: center;
   & > div {
-    display: flex;
-    align-items: center;
     margin-left: 24px;
   }
 `;
 
 const Input = styled.input`
   width: 100%;
-  height: 100%;
-  margin-top: 2px;
+  height: 80%;
   margin-left: 12px;
   outline: none;
   border: none;
