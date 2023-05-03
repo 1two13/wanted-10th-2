cabebae4dda967124d5959134372287ce0ec7f71
Tory <88531407+1two13@users.noreply.github.com>
2023-05-04 00:26:57 +0900

    feat: proxy 설정

--- src/static/constants.ts
+++ src/static/constants.ts
@@ -1,4 +1,5 @@
+const BASE_URL = '/api/v1/search-conditions';
 const FIRST_TITLE = '국내 모든 임상시험 검색하고';
 const SECOND_TITLE = '온라인으로 참여하기';
 
-export { FIRST_TITLE, SECOND_TITLE };
+export { BASE_URL, FIRST_TITLE, SECOND_TITLE };
